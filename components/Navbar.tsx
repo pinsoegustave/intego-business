9c241ed464390415ec6ac36f10b7d627b1fd1177
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-22 22:43:52 -0500

    Update global css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='bg-[white] '>Navbar</div>
+    <nav className='border-2 border-red-200 flexBetween max-container padding-container relative z-30 py-5'>
+        <Link href={"/"}>
+            <h1 className='uppercase'>intego</h1>
+        </Link>
+    </nav>
   )
 }
