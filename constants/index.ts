c9f299980018fbfd7f8732f47bc0869c4e9bc0b8
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-22 21:02:08 -0500

    Added constantws

---
+++ constants/index.ts
@@ -0,0 +1,59 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    {
+        href: '/',
+        key: "home",
+        label: "Home",
+    },
+    {
+        href: '/',
+        key: "about",
+        label: "About us",
+    },
+    {
+        href: '/',
+        key: "services",
+        label: "Services",
+    },
+    {
+        href: '/',
+        key: "contact",
+        label: "Contact",
+    },
+];
+
+export const FOOTER_LINKS = [
+    {
+        title: "Service Menu",
+        links: [
+            'Mistakes to Avoid',
+            'Your Startup',
+            'Ikibina',
+            'Consultancy'
+        ],
+    },
+    {
+        title: "About Us",
+        links: [
+            'Latest News',
+            'Careers',
+            'General Inquiries',
+            'Case Studies'
+        ]
+    },
+    {
+        title: "Contact Us",
+        links: [
+            'X/ Twitter',
+            'Email',
+            'Instagram',
+            'Facebook'
+        ]
+    }
+];
+
+export const terms = [
+    'Terms & Condition',
+    'Privacy Policy',
+    'Contact Us'
+]
