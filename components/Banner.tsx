7141b6348019df293c633ab3ad503ba13779ef6e
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-11-26 12:11:07 -0500

    Start about page

---
+++ components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <section className='w-[96%] mx-auto bg-[#034833] h-64 text-white rounded-4xl mt-10'>
+        <div className='max-container padding-container h-full flex flex-col justify-center gap-8'>
+            <h1 className='regular-64'>About Us</h1>
+            <p> Home &gt; About Us</p>
+        </div>
+    </section>
+  )
+}
